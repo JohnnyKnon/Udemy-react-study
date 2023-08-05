a9654d29d9af5cd858e1baab87711dc01e6e5649
JohnnyKnon <johnyhong24@gmail.com>
2023-08-05 17:26:28 +0900

    keyConceptsImage 헤더 컴포넌트에서 고정

--- 02-example-site/src/components/Header/Header.js
+++ 02-example-site/src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import './Header.css';
+import keyConceptsImage from '../../assets/images/key-concepts.png';
 
 const Header = (props) => {
-  const headImageSrc = props.headImage;
   return (
     <header>
-      <img src={headImageSrc} alt="Medal badge with a star" />
+      <img src={keyConceptsImage} alt="Medal badge with a star" />
       <h1>Key React Concepts</h1>
       <p>Selected key React concepts you should know about</p>
     </header>
